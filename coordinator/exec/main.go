39864361f5f27327253de27c54570fd13eebb353
unicodesnowman <utfsnowman@gmail.com>
2017-07-07 12:08:34 -0400

    Add package level databaseconsumer var

--- coordinator/exec/main.go
+++ coordinator/exec/main.go
@@ -5,11 +5,14 @@ import (
 	"pluralsight.com/coordinator"
 )
 
+var dc *coordinator.DatabaseConsumer
+
 func main() {
 	fmt.Println("Starting coordinator")
 	ea := coordinator.NewEventAggregator()
-	//dc := coordinator.NewDatabaseConsumer(ea)
+	dc = coordinator.NewDatabaseConsumer(ea)
 	ql := coordinator.NewQueueListener(ea)
+
 	go ql.ListenForNewSource()
 
 	fmt.Println("Listening for new sources...")
